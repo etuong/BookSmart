8a2fb1f4a8044931cc0ba8098f1dd591bfa11c5b
etuong <etuong@gmail.com>
2020-11-27 18:33:32 -0500

    Init Commit

---
+++ src/main/java/com/booksmart/BookSmartApplication.java
@@ -0,0 +1,13 @@
+package com.booksmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSmartApplication.class, args);
+	}
+
+}
