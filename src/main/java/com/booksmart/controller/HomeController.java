225da9e7e0723d1e08d3782a49ff1681af0cef9e
etuong <etuong@gmail.com>
2020-11-27 23:18:20 -0500

    Updated templates

--- src/main/java/com/booksmart/controller/HomeController.java
+++ src/main/java/com/booksmart/controller/HomeController.java
@@ -9,4 +9,49 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @RequestMapping("/faq")
+    public String faq() {
+        return "faq";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/product")
+    public String product() {
+        return "product";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @RequestMapping("/shipping")
+    public String shipping() {
+        return "shipping";
+    }
+
+    @RequestMapping("/shop")
+    public String shop() {
+        return "shop";
+    }
+
+    @RequestMapping("/terms-conditions")
+    public String termsConditions() {
+        return "terms-conditions";
+    }
+
+    @RequestMapping("/checkout")
+    public String checkout() {
+        return "checkout";
+    }
 }
